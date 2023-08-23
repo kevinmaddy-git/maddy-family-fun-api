38e1c986cfaf549e65b55304435658a5a0dbf901
kevinmaddy-git <kevbenefitsyou@gmail.com>
2023-08-22 21:18:51 -0400

    Adjusted functionality of shop.  Cart not finished.  Added a DB from MySQL

--- routes/shop.js
+++ routes/shop.js
@@ -1,26 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
-const dataPath = path.join(__dirname, '..', 'data', 'shop.json');
-const rawData = fs.readFileSync(dataPath);
-const data = JSON.parse(rawData);
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'rootroot',
+  database: 'maddy_shop'
+});
 
-function getShopById(id) {
-  return data.find((shopItem) => shopItem.id === id.toString());
-}
+// Get all shop items
+router.get('/shop', (req, res) => {
+    connection.query('SELECT * FROM shop_items', (err, results) => {
+      if (err) {
+        console.error('Error fetching shop items:', err);
+        return res.status(500).json({ error: 'Error fetching shop items' });
+      }
+      console.log('Fetched shop items:', results);
+      res.json(results);
+    });
+  });
 
-// Route to get item by id
-router.get('/shop/:id', (req, res) => {
-  const shopId = req.params.id;
-  const shop = getShopById(shopId);
+// Add item to cart
+router.post('/cart', (req, res) => {
+  const { item_id, quantity } = req.body;
+  connection.query(
+    'INSERT INTO cart_items (item_id, quantity) VALUES (?, ?)',
+    [item_id, quantity],
+    (err, result) => {
+      if (err) throw err;
+      res.json({ message: 'Item added to cart' });
+    }
+  );
+});
 
-  if (shop) {
-    res.json(shop);
-  } else {
-    res.status(404).json({ error: 'Info not found' });
-  }
+// Get cart items
+router.get('/cart', (req, res) => {
+  connection.query(
+    'SELECT cart_items.id, shop_items.name, shop_items.price, cart_items.quantity FROM cart_items INNER JOIN shop_items ON cart_items.item_id = shop_items.id',
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
 });
 
 module.exports = router;
